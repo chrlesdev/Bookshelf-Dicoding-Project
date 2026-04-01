32e42be71b6706df994ec015a083f5152a4c86d6
chrlesdev <charlescornelio0813@gmail.com>
2026-04-01 20:55:16 +0700

    part 6: adding search bar function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let books = [];
 const RENDER_EVENT = "render-book";
 const localStorageKey = "BooksAreQuietMiracles";
+let searchKeyword = "";
 
 if (typeof Storage !== undefined) {
   if (localStorage.getItem(localStorageKey) === null) localStorage.getItem(localStorageKey, 0);
@@ -31,11 +32,11 @@ function addBooks() {
   const isCompleted = document.getElementById("bookFormIsComplete").checked;
 
   const generatedBookID = generatedBookId();
-  const booksObject = generatedBookForm(generatedBookID, bookTitle, bookAuthor, bookYear, isCompleted);
+  const booksObject = generatedBookForm(generatedBookID, bookTitle, bookAuthor, Number(bookYear), isCompleted);
 
   //cek dlu input gak boleh kosong
   if (!bookTitle.trim() || !bookAuthor.trim() || !bookYear) {
-    alert("please fill in the form");
+    alert("please fill in the form Correctly ");
   } else {
     books.push(booksObject);
     document.dispatchEvent(new Event(RENDER_EVENT));
@@ -64,27 +65,35 @@ document.addEventListener(RENDER_EVENT, function () {
   completedBooks.innerHTML = "";
 
   for (const booksItem of books) {
-    const bookElement = createNewBookList(booksItem);
-    if (booksItem.completed === false) {
-      uncompleteBooks.append(bookElement);
-    } else {
-      completedBooks.append(bookElement);
+    const isMatched = booksItem.title.toLowerCase().includes(searchKeyword);
+    if (isMatched) {
+      const bookElement = createNewBookList(booksItem);
+      if (booksItem.completed === false) {
+        uncompleteBooks.append(bookElement);
+      } else {
+        completedBooks.append(bookElement);
+      }
     }
   }
 });
 
 function createNewBookList(bookValue) {
   const bookTitle = document.createElement("h3");
+  bookTitle.setAttribute("data-testid", "bookItemTitle");
+  bookTitle.classList.add("book-title");
   bookTitle.innerText = bookValue.title;
 
   const bookAuthor = document.createElement("p");
-  bookAuthor.innerText = bookValue.author;
+  bookAuthor.setAttribute("data-testid", "bookItemAuthor");
+  bookAuthor.innerText = `Penulis: ${bookValue.author}`;
 
   const bookYear = document.createElement("p");
-  bookYear.innerText = bookValue.year;
+  bookYear.setAttribute("data-testid", "bookItemYear");
+  bookYear.innerText = `Tahun: ${bookValue.year}`;
 
   const buttonDelete = document.createElement("button");
   buttonDelete.innerText = "hapus buku";
+  buttonDelete.setAttribute("data-testid", "bookItemDeleteButton");
   buttonDelete.addEventListener("click", function (e) {
     e.preventDefault();
     deleteBook(bookValue.id);
@@ -92,6 +101,7 @@ function createNewBookList(bookValue) {
 
   const buttonEdit = document.createElement("button");
   buttonEdit.innerText = "edit buku";
+  buttonEdit.setAttribute("data-testid", "bookItemEditButton");
   buttonEdit.addEventListener("click", function (e) {
     e.preventDefault();
     editBook(bookValue.id);
@@ -102,13 +112,15 @@ function createNewBookList(bookValue) {
   textContainer.append(bookTitle, bookAuthor, bookYear, buttonDelete, buttonEdit);
 
   const container = document.createElement("section");
-  container.classList.add("item", "shadow");
+  container.setAttribute("data-bookid", bookValue.id);
+  container.setAttribute("data-testid", "bookItem");
   container.append(textContainer);
   container.setAttribute("id", `books-${bookValue.id}`);
 
   if (bookValue.completed) {
     const buttonComplete = document.createElement("button");
     buttonComplete.innerText = "selesai dibaca";
+    buttonComplete.setAttribute("data-testid", "bookItemIsCompleteButton");
     buttonComplete.addEventListener("click", function (e) {
       e.preventDefault();
       inCompletedBooks(bookValue.id);
@@ -118,6 +130,7 @@ function createNewBookList(bookValue) {
   } else {
     const buttoninComplete = document.createElement("button");
     buttoninComplete.innerText = "belum selesai dibaca";
+    buttoninComplete.setAttribute("data-testid", "bookItemIsCompleteButton");
     buttoninComplete.addEventListener("click", function (e) {
       e.preventDefault();
       completedBooks(bookValue.id);
@@ -183,3 +196,12 @@ function findBookIndex(bookId) {
   }
   return -1;
 }
+
+const search = document.getElementById("searchBookTitle");
+
+search.addEventListener("keyup", function (e) {
+  searchKeyword = e.target.value.toLowerCase();
+  document.dispatchEvent(new Event(RENDER_EVENT));
+});
+
+//design tak perlu, system jalan nomor 1 🙏
